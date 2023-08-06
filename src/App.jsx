46e77a87d0689cb4fe4cbc17d6a3cdf0e172fcc0
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-08-06 16:36:21 +0100

    up to the first page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createSignal } from "solid-js";
+import banner from "./assets/banner.png";
+import Card from "./components/Card";
 
 function App() {
+  const [darkTheme, setDarkTheme] = createSignal(false);
+
+  function toggleTheme() {
+    setDarkTheme(!darkTheme())
+  }
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div class="container m-auto">
+        <header
+          class="my-4 p-2 text-xl flex items-center gap-4"
+          classList={{"bg-neutral-900": darkTheme(), "text-white": darkTheme()}}
         >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+          <span class="material-symbols-outlined cursor-pointer" onClick={toggleTheme} >
+            light_mode
+          </span>
+          <h1>Kaizen Shop</h1>
+        </header>
+
+        <img class="rounded-md" src={banner} alt="" />
+        <div class="grid grid-cols-4 gap-10 my-4">
+          {/*<Card title="Red Tee"/>*/}
+
+          <Card rounded={true} flat={false}>
+            <h2>Green Tee</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              dolorem, inventore qui debitis alias pariatur!
+            </p>
+            <button class="btn">View</button>
+          </Card>
+          <Card rounded={false} flat={true}>
+            <h2>Blue Tee</h2>
+            <button class="btn">View</button>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              dolorem, inventore qui debitis alias pariatur!
+            </p>
+            <p>Only $9.99</p>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
 
